734f9a151c099c4d2bd7834b975abc94b2d9ec10
charleshogsett <polowearingguy@gmail.com>
2025-06-09 21:47:04 -0400

    complete callback hell. Finish read me.

--- callbackVersion.js
+++ callbackVersion.js
@@ -1,18 +1,92 @@
 
-function mowYard(callback){
-    set timeout(() => {
-        const didSleepMow = Math.random() < .2;
-        if (didSleepMow > 0) {
-            console.log("Charlie mowed the yard");
+function mowYard(name, callback){
+    setTimeout(() => {
+            console.log(`${name} mowed the yard`);
+            callback();
+        }, 
+        
+     2000);
+    
+}
+
+function weedEat(name, callback){
+    setTimeout(() => {
+        const didSleep = Math.random() < .1;
+        if (!didSleep) {
+            console.log(`${name} finished using the weed eater.`);
+            callback();
         } 
         else {
-            console.log("Charlie fell asleep after mowing the yard")
+            console.log(`${name} fell asleep after mowing the yard`)
 
         }
-    }, 2000
+        
+    }, 1500
     )
 }
 
-mowYard() => {
-    
+function trimHedges(name, callback){
+    setTimeout(() => {
+        const didSleep = Math.random() < .1;
+        if (!didSleep) {
+            console.log(`${name} finished trimming the hedges.`);
+            callback();
+        } 
+        else {
+            console.log(`${name} fell asleep after weed eating the yard.`)
+
+        }
+        
+    }, 1000
+    )
 }
+
+function collectWood(name, callback){
+    setTimeout(() => {
+        const didSleep = Math.random() < .2;
+        if (!didSleep) {
+            console.log(`${name} finished collecting wood.`);
+            callback();
+        } 
+        else {
+            console.log(`${name} fell asleep after trimming the hedges.`)
+
+        }
+        
+    }, 2500
+    )
+}
+
+function waterGarden(name, callback){
+    setTimeout(() => {
+        const didSleep = Math.random() < .3;
+        if (!didSleep) {
+            console.log(`${name} finished watering the garden.`);
+            callback();
+        } 
+        else {
+            console.log(`${name} fell asleep after collecting wood.`)
+
+        }
+        
+    }, 500
+    )
+}
+
+function doSummerChores(name) {
+    mowYard(name, () => {
+        weedEat(name, () => {
+            trimHedges(name, () => {
+                collectWood(name, () => {
+                    waterGarden(name, () => {
+    console.log(`${name} finished all their chores!`);
+                    });
+                 });
+            });
+        });
+    });
+}
+
+const name = process.argv[2] || "Charlie";
+
+doSummerChores(name);
